e30c7cb4d1382c5a5ace50f224c09693ab2a2546
Laura Hackney <laura@anniecannons.com>
2023-08-05 11:11:00 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,107 @@
+/************************** Exercise 1 ***********************/
+
+// List five different examples of a website using a conditional statement. 
+
+
+
+
+
+
+
+
+/************************** Exercise 2 ***********************/
+
+let itemToBuy = " ";
+let savings = 100;
+if ( savings > 500 ) {
+  itemToBuy = "Computer";
+} else if ( savings > 200 ) {
+ itemToBuy = "Phone";
+} else if ( savings > 0 ) {
+ itemToBuy = "Dinner";
+} else {
+ itemToBuy = "...still saving...";
+}
+
+// Based on the above code, what will be the value of itemToBuy after the code is run. How will you check your answer to be sure it is correct?
+
+
+
+
+
+
+/************************** Exercise 3 ***********************/
+
+let userResponse = prompt("Give me a number, any number, and I will tell you whether it is positive or negative.");
+
+
+// Create a conditional statement that will check to see if the number is positive or negative. Alert the user if the number is positive or negative. 
+
+
+
+
+
+
+
+
+/************************** Exercise 4 ***********************/
+
+const wholesalePrice = 5.45;
+const retailPrice = 9.99;
+const quantity = 47;
+const salesTotal = retailPrice * quantity;
+const profit = salesTotal - (wholesalePrice * quantity);
+
+
+// Write a conditional statement for your online store. If the profit is greater than $400, log to the console, 'You are good for today!'. If the profit is less than or equal to $400, log the string, 'You need to keep working!'
+
+
+
+
+
+
+
+/************************** Exercise 5 ***********************/
+
+// Write a function that takes three numbers as arguments and returns the largest number among them.
+
+
+
+
+
+
+
+
+
+/************************** Exercise 6 ***********************/
+function evenOrOdd(number) {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+// Call this function so that you have a variable with the value of 'Even'
+
+
+
+// Call this function so that you have a variable with the value of 'Even'
+
+
+
+
+
+
+/************************** Exercise 7 ***********************/
+// Create a function that takes in one argument for password. Check to see if the user's password is longer than 12 characters. If it is, alert 'Welcome', if it is not, alert 'You need to enter a longer password.'
+
+
+
+
+
+
+
+/************************** Exercise 8 ***********************/
+// Create a function that takes in two arguments for password and confirm password. Check to see if the user's passwords are the exact same string. If it is, alert 'Welcome', if it is not, alert 'Your passwords do not match.'
+
